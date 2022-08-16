9c99eae0da39ae911ad94bfe8bd0c07f5189901a
saidugurlu <ugurlumsaid@gmx.de>
2022-08-16 10:20:43 +0200

    user added in server.js

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ import express from 'express';
 const app = express();
 const PORT = 3024;
 
+const users = [
+    {
+        username: 'ja',
+        firstName: 'Jörg',
+        lastName: 'Ackermann',
+        email: 'ja@mail.com'
+    },
+    {
+        username: 'ac',
+        firstName: 'Angelika',
+        lastName: 'Carstense',
+        email: 'ac@mail.com'
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('session/cookie basic test');
 });
